62cbba8271130f0809928a0a485365c7108ff494
LupusAnay <lupusanay@gmail.com>
2018-02-17 23:43:55 +0300

    FEATURE Тестовые маршрутизации, глобальные-конфиг переменные для подключения к бд

--- index.php
+++ index.php
@@ -1,86 +1,33 @@
 <?php
 
 // Kickstart the framework
-$f3=require('lib/base.php');
+$f3 = require('lib/base.php');
 
-$f3->set('DEBUG',1);
-if ((float)PCRE_VERSION<7.9)
-	trigger_error('PCRE version is out of date');
+$f3->set('DEBUG', 1);
+if ((float)PCRE_VERSION < 7.9)
+    trigger_error('PCRE version is out of date');
 
 // Load configuration
 $f3->config('config.ini');
 
-$f3->route('GET /',
-	function($f3) {
-		$classes=array(
-			'Base'=>
-				array(
-					'hash',
-					'json',
-					'session',
-					'mbstring'
-				),
-			'Cache'=>
-				array(
-					'apc',
-					'apcu',
-					'memcache',
-					'memcached',
-					'redis',
-					'wincache',
-					'xcache'
-				),
-			'DB\SQL'=>
-				array(
-					'pdo',
-					'pdo_dblib',
-					'pdo_mssql',
-					'pdo_mysql',
-					'pdo_odbc',
-					'pdo_pgsql',
-					'pdo_sqlite',
-					'pdo_sqlsrv'
-				),
-			'DB\Jig'=>
-				array('json'),
-			'DB\Mongo'=>
-				array(
-					'json',
-					'mongo'
-				),
-			'Auth'=>
-				array('ldap','pdo'),
-			'Bcrypt'=>
-				array(
-					'mcrypt',
-					'openssl'
-				),
-			'Image'=>
-				array('gd'),
-			'Lexicon'=>
-				array('iconv'),
-			'SMTP'=>
-				array('openssl'),
-			'Web'=>
-				array('curl','openssl','simplexml'),
-			'Web\Geo'=>
-				array('geoip','json'),
-			'Web\OpenID'=>
-				array('json','simplexml'),
-			'Web\Pingback'=>
-				array('dom','xmlrpc')
-		);
-		$f3->set('classes',$classes);
-		$f3->set('content','welcome.htm');
-		echo View::instance()->render('layout.htm');
-	}
+$f3->set('DB',
+    new DB\SQL (
+        'mysql:host=localhost;port=3306;dbname=local_base_for_testing',
+        'root',
+        ''
+    )
 );
 
-$f3->route('GET /userref',
-	function($f3) {
-		$f3->set('content','userref.htm');
-		echo View::instance()->render('layout.htm');
-	}
+$f3->route('GET /',
+    function () {
+        echo('hello, it\'s root');
+    }
+);
+
+$f3->route('GET /db',
+    function () {
+        echo('hello, it\'s db');
+    }
 );
 
 $f3->run();
