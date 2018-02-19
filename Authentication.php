d4b72714361cf8f4b789691c1832de78efbd0539
LupusAnay <lupusanay@gmail.com>
2018-02-19 16:05:07 +0300

    FEATURE Изменена директория класса аутентификации

---
+++ Authentication.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lupusanay
+ * Date: 19.02.2018
+ * Time: 13:51
+ */
+
+class Authentication
+{
+    public static function register($f3, $params) {
+
+    }
+
+    public static function login() {
+        //TODO Сделать это
+    }
+
+}
