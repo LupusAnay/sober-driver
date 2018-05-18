9be10fc86593c25fc2f4f0ed40733ca6b895009a
Арсений <arseniyyignatev99@gmail.com>
2018-05-18 15:54:49 +0300

    FEATURE вместо таблицы выполненных заказов теперь используется статус выполнения в таблице заказов

--- Complete.php
+++ Complete.php
@@ -41,15 +41,6 @@ class Complete
 
     static function afterroute(Base $f3)
     {
-        $result = $f3->get('DB')->exec("SELECT * FROM orders WHERE client_complete_check = 'true' AND driver_complete_check = 'true'");
-        $json_res = json_encode($result);
-        $json_code = json_decode($json_res, true);
-        $completed_order_data = array($json_code[0]['id'], $json_code[0]['date'], $json_code[0]['value'], $json_code[0]['driver_phone'], $json_code[0]['client_number']);
-        if (count($result) != 0) {
-            $f3->get('DB')->exec('INSERT INTO executed_orders(order_id, date, value, driver_phone, client_number) VALUE(?, ?, ?, ?, ?)',
-                $completed_order_data
-            );
-            $f3->get('DB')->exec("DELETE FROM orders WHERE client_complete_check = 'true' AND driver_complete_check = 'true'");
-        }
+        $f3->get('DB')->exec("UPDATE orders SET status = 'complete' WHERE client_complete_check = 'true' AND driver_complete_check = 'true'");
     }
 }
